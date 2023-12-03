6358265bff2da254448dea9d25315125f05698f6
Renato <renato.farruggio@stud.unibas.ch>
2023-12-04 00:59:46 +0100

    Change folder name and html file name in script 3

--- script_3_handle_submissions_with_accept.py
+++ script_3_handle_submissions_with_accept.py
@@ -1,10 +1,11 @@
 import os
 import shutil
 
-def copy_folders_with_accept_file(base_data_path, source_subfolder, destination_subfolder):
+def copy_rename_folders_and_html(base_data_path, source_subfolder, destination_subfolder):
     """
-    Copies all folders that contain 'accept.txt' from the source subfolder within the base data folder 
-    to a new destination subfolder, asks for user confirmation if the destination subfolder already exists.
+    Copies folders containing 'accept.txt' from the source subfolder within the base data folder 
+    to a new destination subfolder, renames the folders, and renames the .html file in each folder to 'index.html'.
+    Prints the folder name if there isn't exactly one .html file.
     """
 
     # Define source and destination paths using the base data path
@@ -31,16 +32,27 @@ def copy_folders_with_accept_file(base_data_path, source_subfolder, destination_
     # Create the destination folder
     os.makedirs(destination_path, exist_ok=True)
 
-    # Copy only folders that contain 'accept.txt'
+    # Copy and rename folders, and rename .html file if it exists
     for folder_name in os.listdir(source_path):
         source_folder = os.path.join(source_path, folder_name)
         accept_file_path = os.path.join(source_folder, 'accept.txt')
-        destination_folder = os.path.join(destination_path, folder_name)
 
         if os.path.isdir(source_folder) and os.path.exists(accept_file_path):
+            # Split the folder name, remove last two elements, and rejoin
+            new_folder_name = '_'.join(folder_name.split('_')[:-2])
+            destination_folder = os.path.join(destination_path, new_folder_name)
+
             shutil.copytree(source_folder, destination_folder)
 
-    print("Folders with 'accept.txt' copied to destination successfully.")
+            # Rename .html file to 'index.html' if exists
+            html_files = [f for f in os.listdir(destination_folder) if f.endswith('.html')]
+            if len(html_files) == 1:
+                os.rename(os.path.join(destination_folder, html_files[0]), 
+                          os.path.join(destination_folder, 'index.html'))
+            elif len(html_files) != 1:
+                print(f"Folder '{new_folder_name}' does not contain exactly one .html file.")
+
+    print("Folders with 'accept.txt' copied, renamed, and .html file processed successfully.")
 
 # Define the base 'data' folder path
 base_data_path = 'data'
@@ -50,4 +62,4 @@ source_subfolder = '2_Extracted'
 destination_subfolder = '3_Accepted'
 
 # Usage
-copy_folders_with_accept_file(base_data_path, source_subfolder, destination_subfolder)
+copy_rename_folders_and_html(base_data_path, source_subfolder, destination_subfolder)
