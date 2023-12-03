ade7a4647353093ab683d25146453c69c8f4f505
Renato <renato.farruggio@stud.unibas.ch>
2023-12-03 23:34:11 +0100

    Fix error in script 2. Works now

--- script_2_extract.py
+++ script_2_extract.py
@@ -1,10 +1,11 @@
 import os
 import shutil
+from zipfile import ZipFile
 
-def extract_files_to_folder(base_data_path, source_subfolder, destination_subfolder):
+def extract_files_and_process_zips(base_data_path, source_subfolder, destination_subfolder):
     """
-    Moves all files from the source folder within the base data folder to a new destination subfolder.
-    Asks for user confirmation if the destination subfolder already exists.
+    Moves all files from the source folder within the base data folder to a new destination subfolder,
+    extracts zip files within each subfolder, and performs additional cleanup and reorganization steps.
     """
 
     # Define source and destination paths using the base data path
@@ -32,14 +33,64 @@ def extract_files_to_folder(base_data_path, source_subfolder, destination_subfol
     os.makedirs(destination_path, exist_ok=True)
 
     # Copy all files from source to destination
-    for file_name in os.listdir(source_path):
-        source_file = os.path.join(source_path, file_name)
-        destination_file = os.path.join(destination_path, file_name)
-        shutil.copytree(source_file, destination_file)
-        
+    for item_name in os.listdir(source_path):
+        source_item = os.path.join(source_path, item_name)
+        destination_item = os.path.join(destination_path, item_name)
 
-    print("Files moved to destination folder successfully.")
+        if os.path.isfile(source_item):
+            shutil.copy(source_item, destination_item)
+        elif os.path.isdir(source_item):
+            shutil.copytree(source_item, destination_item)
 
+
+    # Iterate through each folder in the base path
+    for folder in os.listdir(destination_path):
+        folder_path = os.path.join(destination_path, folder)
+
+        # Check that there is only one file in the folder
+        if len(os.listdir(folder_path)) != 1:
+            print(f"Folder '{folder}' does not contain exactly one file.")
+            continue
+
+        # Find the zip file in the folder
+        zip_files = [f for f in os.listdir(folder_path) if f.endswith('.zip')]
+
+        # Check if there is exactly one zip file
+        if len(zip_files) != 1:
+            print(f"Folder '{folder}' does not contain exactly one zip file.")
+            continue
+
+
+
+        # Extract the zip file
+        zip_file_path = os.path.join(folder_path, zip_files[0])
+        with ZipFile(zip_file_path, 'r') as zip_ref:
+            extract_to = os.path.join(destination_path, folder)  # This should be 'folder1'
+            os.makedirs(extract_to, exist_ok=True)
+            zip_ref.extractall(extract_to)
+            
+            # Delete "__MACOSX" folder if it exists
+            macosx_path = os.path.join(extract_to, '__MACOSX')
+            if os.path.exists(macosx_path):
+                shutil.rmtree(macosx_path)
+
+            # Check and reorganize the contents
+            if not any(file.endswith('.html') for file in os.listdir(extract_to)):
+                if len(os.listdir(extract_to)) == 2:
+                    single_name = os.listdir(extract_to)[0]
+                    single_path = os.path.join(extract_to, single_name)  # This should be 'subfolder1'
+
+                    if os.path.isdir(single_path):
+                        for item in os.listdir(single_path):
+                            shutil.move(os.path.join(single_path, item), extract_to)
+                        os.rmdir(single_path)
+
+        os.remove(zip_file_path)
+
+
+    print("Extraction and cleanup complete.")
+    
+    
 # Define the base 'data' folder path
 base_data_path = 'data'
 
@@ -48,4 +99,4 @@ source_subfolder = 'Original'
 destination_subfolder = 'Extracted'
 
 # Usage
-extract_files_to_folder(base_data_path, source_subfolder, destination_subfolder)
+extract_files_and_process_zips(base_data_path, source_subfolder, destination_subfolder)
