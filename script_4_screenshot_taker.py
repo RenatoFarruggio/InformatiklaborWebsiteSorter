4ce836e4aa14083aff97143efc1eee2367bf2651
Renato <renato.farruggio@stud.unibas.ch>
2023-12-04 01:58:18 +0100

    Update script 4: Skip folders that already contain the screenshot

--- script_4_screenshot_taker.py
+++ script_4_screenshot_taker.py
@@ -22,12 +22,21 @@ def process_folders(base_data_path, source_subfolder, image_name):
     
     counter = 0
     for item in os.listdir(source_path):
-        counter += 1
-        print(f"({counter}/{len(os.listdir(source_path))}) Taking screenshot for {item}.")
         full_path = os.path.join(source_path, item)
-        html_file = os.path.join(full_path, "index.html")
         screenshot_path = os.path.join(full_path, image_name)
+        
+        counter += 1
+
+        # Check if the screenshot already exists
+        if os.path.exists(screenshot_path):
+            print(f"({counter}/{len(os.listdir(source_path))}) Skipping {item}, screenshot already exists.")
+            continue
+
+        print(f"({counter}/{len(os.listdir(source_path))}) Taking screenshot for {item}.")
+        
+        html_file = os.path.join(full_path, "index.html")
         take_screenshot(html_file, screenshot_path)
+
 
 
 # Define the base 'data' folder path
